3ef8af0ed9b3cb08dab6f09aca184b4f27cb94fb
root <youssefmagedan@gmail.com>
2024-01-23 07:05:46 -0800

    a

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""define a Square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """initialize a square
+
+        Args:
+            size(int):snajsfgafgi
+        """
+        self.__size = size
