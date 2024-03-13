401274d9bafcf12c01edc52349991dbe773fa5ca
magoudi <youssefmagedan@gmail.com>
2024-03-13 11:54:34 -0700

    f

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b < 0:
-        a = 1 / a
-        b = abs(b)
-    x = a
-    for i in range(b - 1):
-        if a >= 1:
-            a = a * x
-        else:
-            a = float(a * x)
-    return a
+    return a ** b
