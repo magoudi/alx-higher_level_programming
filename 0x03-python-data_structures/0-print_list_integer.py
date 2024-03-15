592d8b73ec4461c05bd1a009b44089dba0086ffc
magoudi <youssefmagedan@gmail.com>
2024-03-15 07:42:09 -0700

    f

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,6 @@
 
 def print_list_integer(my_list=[]):
     """ jfgnndgjnlfkgnlkaqsnfjgn"""
-    
+
     for i in my_list:
         print("{}".format(i))
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    """Adfgadfgadfgadfgadfadfgab"""
+
+    if idx < 0:
+        return None
+    elif idx >= len(my_list):
+        return None
+    else:
+        return my_list[idx]
