8e1029f8d973a2eaaf3463abd61d11eb0eb4e4a3
magoudi <youssefmagedan@gmail.com>
2024-03-16 02:40:45 -0700

    f

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,5 +4,5 @@
 def print_list_integer(my_list=[]):
     """ jfgnndgjnlfkgnlkaqsnfjgn"""
 
-    for i in my_list:
-        print("{}".format(i))
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
