65f89dc4099b186f0b81f26f7c858e4c8dc16f6c
magoudi <youssefmagedan@gmail.com>
2024-03-16 05:19:10 -0700

    f

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,7 +11,7 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head;
 	listint_t *opposite = *head;
-	int n = 0;
+	int n = 1;
 
 	if (*head == NULL)
 		return (1);
@@ -26,7 +26,7 @@ int is_palindrome(listint_t **head)
 		{
 			current = current->next;
 			opposite = *head;
-			for (int j = 0; j < n - i; j++)
+			for (int j = 1; j < n - i; j++)
 				opposite = opposite->next;
 			if (current->n == opposite->n)
 				continue;
