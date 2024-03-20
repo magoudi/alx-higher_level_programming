362f6aad155b4cb3bd56fff5b810e4b054307403
magoudi <youssefmagedan@gmail.com>
2024-03-20 06:13:21 -0700

    f

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = list(my_list)
-    new_list[new_list.index(search)] = replace
-    return new_list
+    try:
+        while True:
+            new_list[new_list.index(search)] = replace
+    except ValueError:
+        return new_list
