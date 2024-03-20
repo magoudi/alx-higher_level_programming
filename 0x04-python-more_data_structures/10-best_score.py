4d1a7d339d2280dfa449b8c49173c7e66e6d5baa
magoudi <youssefmagedan@gmail.com>
2024-03-20 10:45:42 -0700

    f

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    first_key = next(iter(a_dictionary))
-    first_value = a_dictionary[first_key]
-    for i in a_dictionary.values():
-        if i > first_value:
-            first_value = i
-    return first_value
+    if not a_dictionary:
+        return None
+    Max = 0
+    for key, value in a_dictionary.items():
+        if value > Max:
+            Max = value
+    for key, value in a_dictionary.items():
+        if value == Max:
+            return key
