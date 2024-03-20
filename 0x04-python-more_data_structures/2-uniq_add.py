e20031c1e62b3bacd96cb3b55c50288bce54873f
magoudi <youssefmagedan@gmail.com>
2024-03-20 06:29:14 -0700

    f

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    x = my_list[0]
+    for i in range(1, len(my_list)):
+        f = 1
+        for j in range(i):
+            if my_list[i] == my_list[j]:
+                f = 0
+        if f == 1:
+            x += my_list[i]
+    return x
