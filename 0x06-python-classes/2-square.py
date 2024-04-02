e17a5ffbfbfb7b0a921fe4aed7ddbbc487d7d263
magoudi <youssefmagedan@gmail.com>
2024-04-02 12:27:51 -0700

    f

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,9 +11,9 @@ class Square:
         Args:
             size (int): wfgfgargasrdgadgadg
         """
-        if(not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif(size < 0):
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
