f1588c6ca5f0df8a7744cd2137069488db430a02
magoudi <youssefmagedan@gmail.com>
2024-04-02 05:34:37 -0700

    f

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/ussr/bin/python3
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except Exception:
+    except TypeError:
         return False
