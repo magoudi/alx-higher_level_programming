3f68b6347aba0f77296758cce441c0a10eaee0da
magoudi <youssefmagedan@gmail.com>
2024-04-09 10:05:16 -0700

    f

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -7,3 +7,12 @@ print(my_rectangle.__dict__)
 my_rectangle.width = 10
 my_rectangle.height = 3
 print(my_rectangle.__dict__)
+
+try: 
+    myrectangle = Rectangle(2, -3) 
+except Exception as e: 
+    print("{}---".format(e))
+try: 
+    myrectangle = Rectangle(-2, 3) 
+except Exception as h:
+    print("---{}".format(h))
