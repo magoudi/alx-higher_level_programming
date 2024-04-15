23f386d43199ebd4996408373ada52c1df5aa90c
magoudi <youssefmagedan@gmail.com>
2024-04-15 13:08:11 -0700

    f

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""hgfyrdtrdtyfghioiluursred"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """fdtfubybtddhtgkujrdryf"""
+
+    def __init__(self, size):
+        """tfytftfuguygiyhiygi"""
+        super().__init__(size, size)
