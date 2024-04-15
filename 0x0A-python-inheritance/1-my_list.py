fc09ed64286fcb5dc9c20459acfb2c06cb4277b1
magoudi <youssefmagedan@gmail.com>
2024-04-15 10:44:23 -0700

    f

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-
-"""sgfvsbrsghdfbvdhfj"""
+"""
+===========================
+Module with class MyList
+===========================
+"""
 
 
 class MyList(list):
-    """fvbcdfjhdvbfjhdbsfd"""
+    """Class with method print_sorted"""
     pass
 
     def print_sorted(self):
-        """sdfgvnskdjfbgkjfnv"""
+        """Methot that sorted a list"""
+
         print(sorted(list(self)))
