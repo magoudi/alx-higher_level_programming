fd05097e80f79d134d69662a40ec1766e3c8bbbe
magoudi <youssefmagedan@gmail.com>
2024-04-15 10:26:58 -0700

    f

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+
+"""sgfvsbrsghdfbvdhfj"""
+
+
+class MyList(list):
+    """fvbcdfjhdvbfjhdbsfd"""
+
+    def print_sorted(self):
+        """sdfgvnskdjfbgkjfnv"""
+
+        print(sorted(self))
