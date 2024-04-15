87ae34594d2d9fc8f5730abd4eaa677d3e251693
magoudi <youssefmagedan@gmail.com>
2024-04-15 11:39:24 -0700

    f

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""sfgbfgbsgdhgghsrtdfgvgbsfgdf"""
+
+
+class Rectangle(BaseGeometry):
+    """dtghbdrstdhgvsdfhdsghdghfbv"""
+
+    def __init__(self, width, height):
+        """sfdbvsdfgvsdfbvdsfbvsdgbf"""
+        try:
+            self.integer_validator("width", width)
+            self.integer_validator("height", height)
+            self.__width = width
+            slf.__height = height
+        except Exception:
+            pass
