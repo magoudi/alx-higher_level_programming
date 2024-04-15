01368dadd29e84c22811310498ef07d5683e2a69
magoudi <youssefmagedan@gmail.com>
2024-04-15 12:06:14 -0700

    f

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,7 +17,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Method for validate if a num is integer"""
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
