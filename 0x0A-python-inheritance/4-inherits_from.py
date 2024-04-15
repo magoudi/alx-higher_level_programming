235facf43b5379480fcf37664b2cfe3338ebddfa
magoudi <youssefmagedan@gmail.com>
2024-04-15 13:38:45 -0700

    f

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,4 +5,7 @@
 
 def inherits_from(obj, a_class):
     """sgdbvsdfhsdfdsfdfsb"""
-    return False if type(obj) is a_class else issubclass(type(obj), a_class)
+    if type(obj) is a_class:
+        return False 
+    else:
+        return issubclass(type(obj), a_class)
