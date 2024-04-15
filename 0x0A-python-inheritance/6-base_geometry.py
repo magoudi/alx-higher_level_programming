a0d90ceb9135a1f3e0079476a27b881f03b5b76f
magoudi <youssefmagedan@gmail.com>
2024-04-15 11:17:47 -0700

    f

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Sdghsdfgvsdfhsdfg"""
+
+
+class BaseGeometry:
+    """sdfsdfgsdfbhsdtwetdg"""
+
+    def area(self):
+        """sfsdhsdhsdfhsdfhsdfsdh"""
+        raise Exception("area() is not implemented")
