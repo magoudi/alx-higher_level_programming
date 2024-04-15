3f838cb221bc6d844b8244b7db183104c04b4cdb
magoudi <youssefmagedan@gmail.com>
2024-04-15 11:00:33 -0700

    f

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -5,7 +5,4 @@
 
 def is_same_class(obj, a_class):
     """Sdfgsrtdfgstdfgdtfgsrdtfgdf"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) is a_class
