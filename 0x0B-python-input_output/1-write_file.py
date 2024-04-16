10251e8f5cc5fb1867c2b7be386fe5899a23b28d
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:14:52 -0700

    f

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""vhgcfcffyfuhjkmlk"""
+
+
+def write_file(filename="", text=""):
+    """hgfytdtrsrstrfhrsfshfgfesg"""
+    count = 0
+    with open(filename, "w") as file:
+        for i in text:
+            file.write(i)
+            count += 1
