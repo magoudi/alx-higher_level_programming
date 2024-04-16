0d4028229c59797dd2bc59ef1bea09e1ce9e8a72
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:26:25 -0700

    f

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,6 +7,5 @@ def write_file(filename="", text=""):
     """hgfytdtrsrstrfhrsfshfgfesg"""
     count = 0
     with open(filename, "w") as file:
-        for i in text:
-            file.write(i)
-            count += 1
+        count = file.write(text)
+    return count
