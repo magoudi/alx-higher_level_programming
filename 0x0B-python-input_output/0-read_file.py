651022ea42d10275d69e861723b815f2a2287dec
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:04:41 -0700

    f

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """wtgwdfgsfhgdfgdsfchfgvfgdS"""
     with open(filename, "r") as file:
         content = file.read()
-        print(content, end="")    
+        print(content, end="") 
