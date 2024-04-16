47a6d3c9947eb9752430b7e6d2d36a55ecdca421
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:00:14 -0700

    f

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""sfdnvjkdbkjbkhbdfcnjhdf g,j"""
+
+
+def read_file(filename=""):
+    """wtgwdfgsfhgdfgdsfchfgvfgdS"""
+    with open(filename, "r") as file:
+        content = file.read()
+        print(content)
+    
