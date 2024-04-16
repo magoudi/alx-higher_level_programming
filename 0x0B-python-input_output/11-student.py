92826fa5b0194b3d336172a0b4a89f59eda128ef
magoudi <youssefmagedan@gmail.com>
2024-04-16 13:34:07 -0700

    f

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""fneidnsgfrusdfnciwdnfinfcs"""
+
+
+class Student:
+    """wegwgwegwedhgvwfagwehth"""
+
+    def __init__(self, first_name, last_name, age):
+        """sfdgsdfgwfgwgfvafrg5rgergv"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """wsgrrgveagwfgdbfsdFB"""
+        if (type(attrs) == list and
+                all(type(element) == str for element in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
