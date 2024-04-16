0c57e8f29f886862475184ccd508dbab18f8f42c
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:39:56 -0700

    f

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""sdgnsjdjfnvhjdf  chfhvhngnjs"""
+import json
+
+
+def from_json_string(my_str):
+    """dfvdkjfvnjdfnvjdnfsdgndfjsgn"""
+    data = json.loads(my_str)
+    return data
