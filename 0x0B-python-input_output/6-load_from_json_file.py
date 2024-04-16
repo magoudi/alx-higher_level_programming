9d86de21d46dfb4326f857c6e69067abb65d32f6
magoudi <youssefmagedan@gmail.com>
2024-04-16 12:08:36 -0700

    f

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""dfvsdfcvdfbvdfsgvdfsbvgDF"""
+import json
+
+
+def load_from_json_file(filename):
+    """gvdgvregariufgojfgonfvnisdfgn"""
+    with open(filename, "r") as file:
+        jstr = file.read()
+        return json.loads(jstr)
