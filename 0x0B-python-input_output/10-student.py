1bdbfcb3ccdd72b362f232ff9562fa47598261a5
magoudi <youssefmagedan@gmail.com>
2024-04-16 13:51:40 -0700

    f

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 
-"""fneidnsgfrusdfnciwdnfinfcs"""
+"""Defines a class Student."""
 
 
 class Student:
-    """wegwgwegwedhgvwfagwehth"""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """sfdgsdfgwfgwgfvafrg5rgergv"""
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """wsgrrgveagwfgdbfsdFB"""
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
         if (type(attrs) == list and
                 all(type(element) == str for element in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
