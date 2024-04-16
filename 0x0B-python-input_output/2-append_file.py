5698adece6e19bb9beab3f3a07d28d87a5964c7e
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:28:23 -0700

    f

---
+++ 0x0B-python-input_output/2-append_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""vhgcfcffyfuhjkmlk"""
+
+
+def write_file(filename="", text=""):
+    """hgfytdtrsrstrfhrsfshfgfesg"""
+    count = 0
+    with open(filename, "a") as file:
+        count = file.write(text)
+    return count
