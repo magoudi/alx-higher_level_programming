30883056b48c8d35614c3a621cedbdf0abe0f9bd
magoudi <youssefmagedan@gmail.com>
2024-04-16 11:57:49 -0700

    f

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """rtgnvjndfgsisengdrofcmosdfgd"""
-    jstr = json.dumps(my_obj)
     with open(filename, "w") as file:
+        jstr = json.dumps(my_obj)
         file.write(jstr)
