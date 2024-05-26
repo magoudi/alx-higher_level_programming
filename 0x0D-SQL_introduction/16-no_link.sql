8be7a30dbec0903ee6a136e62375dddb7cce13d2
magoudi <youssefmagedan@gmail.com>
2024-05-26 10:50:58 -0700

    f

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- sjvnsan sfjnl akenfjnkln
-SELECT score, name FROM second_table WHERE name != null ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
