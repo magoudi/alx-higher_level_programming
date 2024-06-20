10aaf36bc87ff596fa76cbc01582929c46c4f73c
magoudi <youssefmagedan@gmail.com>
2024-06-20 04:31:03 +0300

    f

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c) {
+    if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
       }
