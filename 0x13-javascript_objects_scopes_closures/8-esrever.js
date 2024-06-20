c66c8a81518ac6837106d81e79847406e5496755
magoudi <youssefmagedan@gmail.com>
2024-06-20 04:43:09 +0300

    f

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  for (let i = 0; i < list.length / 2; i++) {
+    const temp = list[i];
+    list[i] = list[list.length - 1 - i];
+    list[list.length - 1 - i] = temp;
+  }
+};
