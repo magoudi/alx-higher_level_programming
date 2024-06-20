046ae969cddaaab6230f45e5ecaa86e769dec3ea
magoudi <youssefmagedan@gmail.com>
2024-06-20 04:40:21 +0300

    f

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+let count = 0;
+exports.nbOccurences = function (list, searchElement) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+  return count;
+};
