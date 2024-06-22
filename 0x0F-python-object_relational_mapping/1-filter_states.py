971033953e68d009de96421f50c0c53b61eae1a9
magoudi <youssefmagedan@gmail.com>
2024-06-22 15:54:06 -0700

    f

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+
+"""lsfjglaerjsglasglnvlsrnljeargsV"""
+
+
+from sys import argv
+import MYSQLdb as mdb
+
+if __name__ == "__main__":
+    curr = mdb.connect(host="localhost", port=3306, user=argv[1],
+            passwd=argv[2], db=argv[3], charset="utf8")
+
+    cur = curr.cursor()
+    cur.excute("SELECT * FROM")
