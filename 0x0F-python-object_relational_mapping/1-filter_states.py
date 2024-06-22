dd552dfb9e18a9f657e3bbf42fabe06afda1dc1b
magoudi <youssefmagedan@gmail.com>
2024-06-23 02:01:28 +0300

    f

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ import MYSQLdb as mdb
 
 if __name__ == "__main__":
     curr = mdb.connect(host="localhost", port=3306, user=argv[1],
-            passwd=argv[2], db=argv[3], charset="utf8")
+                       passwd=argv[2], db=argv[3], charset="utf8")
 
     cur = curr.cursor()
     cur.excute("SELECT * FROM states WHERE name LIKE \"N%\" ORDER BY id ASC;")
