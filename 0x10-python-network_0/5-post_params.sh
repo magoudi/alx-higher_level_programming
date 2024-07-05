afa4f48b46564ac6af35485476f89e643c4febda
magoudi <youssefmagedan@gmail.com>
2024-07-05 03:29:19 +0300

    f

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #gvhjugjyfyugy
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
