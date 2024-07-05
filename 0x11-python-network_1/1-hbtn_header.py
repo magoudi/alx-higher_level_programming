d68af720447de19ccfee89c694ea1e37bf292dab
magoudi <youssefmagedan@gmail.com>
2024-07-05 15:54:28 +0300

    f

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-"""Adrgnjanskgnvlajlbnsg"""
+"""Adrgnjaner hretj ertu skeert hr t gnvlajlb ert  reth nsg"""
 from urllib import request
 from sys import argv
 
-with request.urlopen(argv[1]) as resp:
-    print(dict(resp.headers).get("X-Request-Id"))
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as resp:
+        print(dict(resp.headers).get("X-Request-Id"))
