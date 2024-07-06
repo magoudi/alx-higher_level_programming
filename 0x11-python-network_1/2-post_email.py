9b834038048d3bf2c78b7358475689705b87c8e9
magoudi <youssefmagedan@gmail.com>
2024-07-06 04:25:25 +0300

    f

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""Adrgnjaner hretj ertu skeert hr t gnvlajlb ert  reth nsg"""
+from urllib import request, parse
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = parse.urlencode(value).encode("ascii")
+
+    request = request.Request(url, data)
+    with request.urlopen(request) as resp:
+        print(resp.read().decode("utf-8"))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+import sys
+from urllib import request, error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = request.Request(url)
+    try:
+        with request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
